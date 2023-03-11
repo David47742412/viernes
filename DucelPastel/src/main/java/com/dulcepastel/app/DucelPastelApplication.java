590bf10c7a33dd8e083a9c6a556a61d7786130b8
David <davidcastroastudillo10@gmail.com>
2023-03-10 21:29:51 -0500

    cambios

---
+++ DucelPastel/src/main/java/com/dulcepastel/app/DucelPastelApplication.java
@@ -0,0 +1,13 @@
+package com.dulcepastel.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucelPastelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucelPastelApplication.class, args);
+    }
+
+}
---
+++ DucelPastel/src/test/java/com/dulcepastel/app/DucelPastelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulcepastel.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucelPastelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
