2ac24a1249f8b45df4891524ebe8f161398cf7e3
David <davidcastroastudillo10@gmail.com>
2023-03-14 14:35:16 +0500

    cambios

--- Dulcepastel/Dulcepastel/Models/login/Login.cshtml.cs
+++ Dulcepastel/Dulcepastel/Models/login/Login.cshtml.cs
@@ -1,8 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Data;
+using Dulcepastel.Models.usuario;
+using Dulcepastel.Models.utility.context;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace Dulcepastel.Models.login;
 
-public class Login 
+public class Login
 {
-    
+    private string? _email;
+    private string? _password;
+
+    public string? Email
+    {
+        get => _email;
+        set => _email = value;
+    }
+
+    public string? Password
+    {
+        get => _password;
+        set => _password = value;
+    }
+
+    public Usuario? IsValidUser(Login user)
+    {
+        Usuario? usuario = null;
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(DulcepastelContext.Context))
+            {
+                using (SqlCommand command = new SqlCommand("SP_LOGIN", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@Email", SqlDbType.VarChar).Value = user.Email;
+                    connection.Open();
+                    var result = command.ExecuteReader();
+                    string? password = "";
+                    if (result.HasRows)
+                    {
+                        result.Read();
+                        usuario = new Usuario
+                        {
+                            Id = result["usuario_id"] as string,
+                            Nombre = result["usuario_nombre"] as string,
+                            Apellido = result["usuario_apellido"] as string,
+                            Celular = result["usuario_celular"] as string,
+                            Foto = result["usuario_foto"] as string,
+                            FchNacimiento = result["f_nacimiento"] as string
+                        };
+                        password = result["usuario_password"] as string;
+                    }
+                    return BCrypt.Net.BCrypt.Verify(user.Password, password) ? usuario : null;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
 }
---
+++ Dulcepastel/Dulcepastel/Models/usuario/Usuario.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Dulcepastel.Models.usuario;
+
+public class Usuario
+{ 
+    [Key]
+    private string? _id;
+
+    [Required(ErrorMessage = "El Campo Nombre es obligatorio")]
+    private string? _nombre;
+    
+    [Required(ErrorMessage = "El Campo Apellido es obligatorio")]
+    private string? _apellido;
+    
+    [Required(ErrorMessage = "El Campo Celular es obligatorio")]
+    private string? _celular;
+    
+    [Required(ErrorMessage = "El Campo N° Documento es obligatorio")]
+    private string? _nroDoc;
+
+    [Required(ErrorMessage = "El Campo Email es obligatorio")]
+    private string? _email;
+
+    [Required(ErrorMessage = "El Campo Password es obligatorio")]
+    private string? _password;
+    
+    [Required(ErrorMessage = "El Campo Tipo de Documento es obligatorio")]
+    private string? _idTipoDoc;
+    
+    [Required(ErrorMessage = "El Campo Estado es obligatorio")]
+    private string? _idEstado;
+
+    [Required(ErrorMessage = "El Campo Foto es obligatorio")]
+    private string? _foto;
+    
+    [Required(ErrorMessage = "El Campo Ocupacion es obligatorio")]
+    private string? _ocupacion;
+
+    [Required(ErrorMessage = "El Campo Fecha de nacimiento es obligatorio")]
+    private string? _fchNacimiento;
+
+    public string? Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string? Nombre
+    {
+        get => _nombre;
+        set => _nombre = value;
+    }
+
+    public string? Apellido
+    {
+        get => _apellido;
+        set => _apellido = value;
+    }
+
+    public string? Celular
+    {
+        get => _celular;
+        set => _celular = value;
+    }
+
+    public string? NroDoc
+    {
+        get => _nroDoc;
+        set => _nroDoc = value;
+    }
+
+    public string? Email
+    {
+        get => _email;
+        set => _email = value;
+    }
+
+    public string? Password
+    {
+        get => _password;
+        set => _password = value;
+    }
+
+    public string? IdTipoDoc
+    {
+        get => _idTipoDoc;
+        set => _idTipoDoc = value;
+    }
+
+    public string? IdEstado
+    {
+        get => _idEstado;
+        set => _idEstado = value;
+    }
+
+    public string? Foto
+    {
+        get => _foto;
+        set => _foto = value;
+    }
+
+    public string? Ocupacion
+    {
+        get => _ocupacion;
+        set => _ocupacion = value;
+    }
+
+    public string? FchNacimiento
+    {
+        get => _fchNacimiento;
+        set => _fchNacimiento = value;
+    }
+}
